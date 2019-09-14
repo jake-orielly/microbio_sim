edb62c92b6333669d90e183c4430422f2c28cb49
jake-orielly <jake.orielly@gmail.com>
2019-09-14 13:41:50 -0400

    Agent moves around seeking nearest food

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var canvasWidth = 620;
 var canvasHeight = 620;
 var boardWidth = 62;
 var boardHeight = 62;
-var board,newBoard,scale,currNeighbors;
-var intervalTime = 250;
+var board,newBoard,scale;
+var intervalTime = 100;
 
-var colorMap = ['','red']
+var colorMap = ['','red','blue']
 
 function initSimulation() {
     initBoard();
@@ -24,14 +24,32 @@ function clearCanvas() {
 }
 
 function refreshCanvas() {
+    let curr;
     clearCanvas();
     newBoard = [];
-    console.log(board)
     for (let i = 0; i < boardWidth; i++) {
         newBoard[i] = [];
+        for (let j = 0; j < boardHeight; j++)
+            newBoard[i][j] = 0;
+    }
+
+    for (let i = 0; i < boardWidth; i++) {
         for (let j = 0; j < boardHeight; j++) {
-            if (board[i][j] > 0) {
-                ctx.fillStyle = colorMap[board[i][j]];
+            if (board[i][j] != 0) {
+                curr = board[i][j];
+                if (curr.func)
+                    curr.func();
+                newBoard[curr.x][curr.y] = curr;
+            }
+        }
+    }
+    board = newBoard;
+
+    for (let i = 0; i < boardWidth; i++) {
+        for (let j = 0; j < boardHeight; j++) {
+            if (board[i][j] != 0) {
+                curr = board[i][j];
+                ctx.fillStyle = colorMap[curr.val];
                 ctx.fillRect(
                     c.width - ((boardWidth - i) * scale), 
                     c.height - ((boardHeight - j) * scale), 
@@ -39,17 +57,11 @@ function refreshCanvas() {
                     scale
                 );
             }
-            currNeighbors = neighbors(i,j);
-            if (currNeighbors == 2 || currNeighbors == 3)
-                newBoard[i][j] = 1;
-            else
-                newBoard[i][j] = 0;
         }
     }
-    board = newBoard;
 }
 
-function neighbors(x,y) {
+/*function neighbors(x,y) {
     let currX, currY;
     let neighbors = 0;
     for (let i = -1; i < 2; i++)
@@ -62,7 +74,7 @@ function neighbors(x,y) {
                 neighbors++;
         }
     return neighbors;
-}
+}*/
 
 function onBoard(x,y) {
     return (x >= 0 && x < boardWidth &&
@@ -77,8 +89,56 @@ function initBoard() {
         for (let j = 0; j < boardHeight; j++)
             board[i][j] = 0;
     }
-    board[30][30] = 1;
-    board[31][31] = 1;
+    newEntity({x:2,y:2,val:1});
+    newEntity({x:3,y:5,val:2});
+    newEntity({x:3,y:5,val:2});
+    newEntity({x:13,y:25,val:2});
+    newEntity({x:20,y:10,val:2});
+    newEntity({x:35,y:31,val:2});
+}
+
+function newEntity(obj) {
+    let curr = {x:obj.x,y:obj.y,val:obj.val}
+    let next;
+    if (curr.val == 1) {
+        curr.func = function(){
+            if (!this.goal)
+                this.goal = 'food';
+            if (!this.currentGoal)
+                this.currentGoal = this.findClosest(this.goal);
+            next = this.pathFind(this.currentGoal)
+            this.move(next);
+            if (this.x == this.currentGoal.x && this.y == this.currentGoal.y)
+                this.currentGoal = undefined;
+        }
+        curr.findClosest = function(type) {
+            type = 2;
+            for (let searchAreaSize = 1; searchAreaSize < boardHeight/2-1; searchAreaSize++)
+                for (let i = Math.max(this.x - searchAreaSize,0); 
+                i < Math.min(this.x + searchAreaSize + 1, boardWidth); i++)
+                        for (let j = Math.max(this.y - searchAreaSize,0); 
+                        j < Math.min(this.y + searchAreaSize + 1, boardWidth); j++)
+                            if (board[i][j].val == type)
+                                return {x:i,y:j}; 
+            return {x:this.x,y:this.y}
+        }
+        curr.pathFind = function(goal) {
+            let xDiff = goal.x - this.x;
+            let yDiff = goal.y - this.y;
+            if (!(xDiff || yDiff))
+                return {x:0,y:0}
+            else if (Math.abs(xDiff) > Math.abs(yDiff))
+                return {x:(xDiff/Math.abs(xDiff)) * 1, y:0};
+            else
+                return {x:0, y:(yDiff/Math.abs(yDiff)) * 1};
+        }
+        curr.move = function(obj) {
+            this.x += obj.x;
+            this.y += obj.y;
+            board[this.x][this.y] = 0;
+        }
+    }
+    board[obj.x][obj.y] = curr;
 }
 
 function initCanvas() {
